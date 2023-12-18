fcba2e8ba1cf14f551f066933a26cbad6c85e13d
Hasan ÖZCAN <91744709+Hasan26ozcan@users.noreply.github.com>
2023-12-18 14:28:21 +0300

    update_1
    update_1

--- sp_trigger_examples/to_be_delete_field.sql
+++ sp_trigger_examples/to_be_delete_field.sql
@@ -4,5 +4,6 @@
 --DROP PROCEDURE IF EXISTS search_for_surname;
 --DROP PROCEDURE IF EXISTS search_for_surname_and_name;
 --DROP PROCEDURE IF EXISTS search_customer_with_tc;
-DROP PROCEDURE IF EXISTS Insert_Bus_and_voyage_route;
-DROP TRIGGER IF EXISTS update_of_bus_seferýd;
+--DROP PROCEDURE IF EXISTS Insert_Bus_and_voyage_route;
+--DROP TRIGGER IF EXISTS update_of_bus_seferýd;
+DROP TRIGGER IF EXISTS customer_add_database;
--- sp_trigger_examples/çalıştuırma_kodları.sql
+++ sp_trigger_examples/çalıştuırma_kodları.sql
@@ -5,11 +5,17 @@
 --EXEC search_for_surname @surname = '1';
 --EXEC search_for_surname_and_name @surname = '1', @name='1';
 --EXEC search_customer_with_tc @tc= '1';
-EXEC Insert_Bus_and_voyage_route @voyageRouteID=2 , @busID= 1;
+--EXEC Insert_Bus_and_voyage_route @voyageRouteID=2 , @busID= 1;
+
+
 
 
 
 -- tricker place
+
+INSERT INTO customer (tc, name, surname, email, password, phone, customerRoleID)
+VALUES ('12', 'John', 'Doe', 'john.doee@example.com', 'password123', '5551234567', 1);
+
 --DELETE FROM customer
 --WHERE tc = '5';
 
